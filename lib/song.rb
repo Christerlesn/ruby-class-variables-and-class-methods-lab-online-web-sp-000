068f75ba1a51f71c4f24675431ae24c22490c4ec
Christerle Nounoute <christerlesn@gmail.com>
2020-09-30 03:51:55 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+@@count = 0
+@@artists = []
+@@genres = []
+
+
+attr_accessor :name, :artist, :genre
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@count += 1
+end
+
+def count
+  @@count = count
+end
+
+
+
+end
